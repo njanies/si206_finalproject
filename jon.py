ef43f638ad59b2121495c3ec8962b002de4dc45e
njanies <32237245+njanies@users.noreply.github.com>
2019-12-06 15:35:15 -0500

    added spotify api credentials

--- jon.py
+++ jon.py
@@ -34,3 +34,15 @@ if token:
         print(track['name'] + ' - ' + track['artists'][0]['name'])
 else:
     print("Can't get token for", username)
+
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+client_id = bc4107086c834d10a1fa8616f1f4230a
+client_secret = ca5d5a3a7ec741bf9dd9e045117b2730 
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+name = "{Artist Name}" #chosen artist
+result = sp.search(name) #search query
+result['tracks']['items'][0]['artists']
